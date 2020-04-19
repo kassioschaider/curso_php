452664cdae74789a4dfd76b288c6564c857ed9a7
Kassio Schaider <kassio.schaider@hotmail.com>
2020-04-19 15:17:01 -0300

    Operações, strings e tipos.

---
+++ operacoes.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade = 28;
+$idadeDaquiA10Anos = $idade + 10;
+$idadeHa5Anos = $idade - 5;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$potencia = 2 ** 2;
+$restoDaDivisao = 2 % 2;
+
+echo $idadeDaquiA10Anos;
+echo $idadeHa5Anos;
---
+++ tipos.php
@@ -0,0 +1,16 @@
+<?php
+
+$idade = 28;
+$salario = 1680.50;
+$divisao = 10 / 3;
+
+//echo gettype($divisao);
+
+//$divisao = 3;
+
+$texto = "Olá mundo!";
+
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($verdadeiro);
